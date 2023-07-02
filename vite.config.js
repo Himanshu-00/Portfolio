c18721e270f1228b02bb1991fd8bcee7cbf6c4ef
Himanshu-00 <himanshuvinchurkar1387@gmail.com>
2023-07-02 19:02:22 +0530

    Changes

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig({
  
-  base: '/himanshu/',
+  base: '/Portfolio/',
   plugins: [
     react(),
     VitePWA({
