c99fb82b2908e3311e813d577d8a0bd064b5daf7
Himanshu-00 <himanshuvinchurkar1387@gmail.com>
2023-07-05 01:02:39 +0530

    Changes

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -18,7 +18,7 @@ const AboutMe = () => {
     <p className='font-bold'>Vinchurkar Himanshu is a dedicated and ambitious software engineering student with a passion for technology and problem-solving. He excels in programming languages such as C++, and Python and actively seeks practical experience through internships. Himanshu is driven to develop innovative software solutions that improve user experiences and is committed to continuous learning and staying up to date with industry trends. His strong work ethic and motivation make him an ideal candidate for any organization seeking a talented software engineer.
     </p>
     <p className='hover:underline lg:text-sm font-bold'>DMs are alway's open on 
-    <a href='https://www.linkedin.com/in/himanshu-vinchurkar-9b414322b/'> Linkedin</a>
+    <a href='https://www.linkedin.com/in/himanshu-vinchurkar-9b414322b/' target={'_blank'}> Linkedin</a>
     </p>
     </div>
 </div>
