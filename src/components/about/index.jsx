be5e65092a8f07f3f83f49af9fbb0593490db0a0
Himanshu-00 <himanshuvinchurkar1387@gmail.com>
2023-07-19 11:58:00 +0530

    More Mobile Friendly

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -4,25 +4,29 @@ const AboutMe = () => {
   return (
     <div className="card  bg-base-100 shadow-lg bg-opacity-40">
               <div className="card-body">
-                <div className="mx-1 flex justify-between mb-2 py-2">
-                  <h5 className="card-title">
+                <div className="lg:flex justify-between mb-2 py-2">
+                  <h5 className="card-title font-bold">
                     {
-                      <span className="text-base-content opacity-80 lg:mx-2 -mx-3">
+                      <span className="text-base-content opacity-80">
                         Bio
                       </span>
                     }
                   </h5>
-                  { 
-                  <div className="card w-5/6 bg-primary text-primary-content shadow-xl pb-7">
-  <div className="card-body lg:text-lg text-xs">
+                  {
+                 
+                  <div className="card lg:w-5/6 h-96  lg:mt-0 mt-7 bg-primary text-primary-content shadow-xl">
+                    
+  <div className="card-body text-lg lg:overflow-hidden overflow-auto">
     <p className='font-bold'>Vinchurkar Himanshu is a dedicated and ambitious software engineering student with a passion for technology and problem-solving. He excels in programming languages such as C++, and Python and actively seeks practical experience through internships. Himanshu is driven to develop innovative software solutions that improve user experiences and is committed to continuous learning and staying up to date with industry trends. His strong work ethic and motivation make him an ideal candidate for any organization seeking a talented software engineer.
     </p>
     <a href='https://www.linkedin.com/in/himanshu-vinchurkar-9b414322b/' target={'_blank'}>
-    <p className='hover:underline lg:text-sm font-bold cursor-pointer'>DMs are alway's open on Linkedin
+    <p className='hover:underline text-sm font-bold cursor-pointer'>DMs are alway's open on Linkedin
     
     </p></a>
     </div>
 </div>
+
+
 
                   }
                   
