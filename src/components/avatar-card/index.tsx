8edb503a597c7a4b9f3e944b41d501e4f3690cc3
Himanshu-00 <himanshuvinchurkar1387@gmail.com>
2025-02-26 13:09:29 +0530

    update

--- src/components/avatar-card/index.tsx
+++ src/components/avatar-card/index.tsx
@@ -22,7 +22,7 @@ const AvatarCard: React.FC<AvatarCardProps> = ({
       <div className="grid place-items-center py-8">
         {loading || !profile ? (
           <div className="avatar opacity-90">
-            <div className="mb-8 rounded-full w-40 h-40">
+            <div className="mb-8 rounded w-40 h-40">
               {skeleton({
                 widthCls: 'w-full',
                 heightCls: 'h-full',
